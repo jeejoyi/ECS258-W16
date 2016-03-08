1282c319ecfdeebf2761e3a2947ecebff2afb5d1
Hiu Hong Yu <hiuyu@ucdavis.edu>
2016-03-08 00:30:25 -0800

    Added  Plot, deleted plots

--- Server/src/main/java/Plot.java
+++ Server/src/main/java/Plot.java
@@ -1,5 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by CowCow on 3/8/16.
  */
 public class Plot {
+    private JFrame frames;
+    private RealTimeLinePlot charts;
+
+    public Plot(final String windowTitles, final String plotTitles, final String XAxisTitles,
+                 final String YAxisTitles, final float maxYRange, final String[] seriesTitles)   {
+        //set window titles
+        frames = new JFrame(windowTitles);
+        //set default frame closing action
+//        frames.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension d = new Dimension(400,600);
+        frames.setResizable(false);
+
+        //create a realtimeplot
+        charts = new RealTimeLinePlot(plotTitles, XAxisTitles, YAxisTitles, new Date(), maxYRange, seriesTitles.length,
+                                        seriesTitles);
+            //add the chart to the frame
+        frames.add(charts);
+        frames.pack();
+
+        //display frame
+        frames.setVisible(true);
+    }
+    //update all created graph. This function still need to be implemented
+    public void updateGraph()    {
+        Random rand = new Random();
+        float[] data = {rand.nextInt(100)};
+        charts.update(data);
+    }
 }
