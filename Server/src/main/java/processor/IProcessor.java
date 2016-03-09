9eb4f731fe773def548ce730cff5826eeebf0e25
Francesco <capponi.francesco@gmail.com>
2016-03-08 21:07:25 -0800

    Added comment

--- Server/src/main/java/processor/IProcessor.java
+++ Server/src/main/java/processor/IProcessor.java
@@ -6,5 +6,10 @@ import data_type.DataToProcess;
  * Created by fre on 3/8/16.
  */
 public interface IProcessor {
+    /**
+     * Function that must be implemented to handle the Data with a Custom Processor
+     *
+     * @param packet
+     */
     void process(DataToProcess packet);
 }
